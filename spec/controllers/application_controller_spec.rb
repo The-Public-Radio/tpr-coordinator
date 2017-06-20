e0ba89e19e7dc753f7f9e581ab79ef33e0038992
Gabe Ochoa <gabeochoa@gmail.com>
2017-06-19 23:34:26 -0400

    Removing stuff from application controller to use scaffold instead

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,13 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
-  it 'has an index endpoint that returns a 200' do
-    get '/' do
-      explanation 'the index of the application'
-      example_request 'a call to the index endpoint' do
-        expect(response_body).to be 'TPR Coordinator'
-        expect(response_status).to be 200
-      end
-    end
-  end
+
 end
