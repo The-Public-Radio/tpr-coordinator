ee87d58a79b784dcde7686cb76926374183ec63c
Gabe Ochoa <gabeochoa@gmail.com>
2017-06-19 21:36:51 -0400

    Test for index endpoint

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
+  it 'has an index endpoint that returns a 200' do
     get '/' do
-    explanation 'the index of the application'
-    example_request 'what is this' do
-      expect(response_body).to be 'TPR Coordinator'
+      explanation 'the index of the application'
+      example_request 'a call to the index endpoint' do
+        expect(response_body).to be 'TPR Coordinator'
+        expect(response_status).to be 200
+      end
     end
   end
 end
