7bdb98c9e73b45b7b2ed75908b6197f3ba79ab96
Gabe Ochoa <gabeochoa@gmail.com>
2017-06-19 21:26:06 -0400

    Adding in skylight env config and docker-compose w/o keys

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,9 @@ module TprCoordinator
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # configure skylight monitoring
+    # config.skylight.logger = Logger.new(STDOUT)
+    # config.skylight.environments += ["staging"]
   end
 end
