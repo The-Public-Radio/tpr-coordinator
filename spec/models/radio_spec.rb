d6805bb2a9395e692e5815e80a204900b0ceaa83
Gabe Ochoa <gabeochoa@gmail.com>
2017-07-14 19:09:26 -0700

    Small refactor of model tests

--- spec/models/radio_spec.rb
+++ spec/models/radio_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Radio, type: :model do
+  let(:model) { described_class.new(frequency: '98.1') }
+
   it 'valid with valid attributes' do
-    radio = Radio.new(frequency: '98.1')
-    expect(radio).to be_valid
+    expect(model).to be_valid
   end
 
   it 'is not valid without a frequency' do
-    radio = Radio.new(frequency: nil)
-    expect(radio).to_not be_valid
+    model.frequency = nil
+    expect(model).to_not be_valid
   end
 end
