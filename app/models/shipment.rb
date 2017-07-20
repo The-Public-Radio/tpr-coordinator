b5077c5e7e6f882371f8491e775ae3e54453c364
Gabe Ochoa <gabeochoa@gmail.com>
2017-07-20 08:52:50 -0400

    Don't need to validate blank with presence of

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,5 +1,5 @@
 class Shipment < ApplicationRecord
   has_many :radio
   validates_presence_of :tracking_number, with: /1Z/
-  validates_length_of :tracking_number, is: 15, allow_nil: false, allow_blank: false
+  validates_length_of :tracking_number, is: 15, allow_nil: false
 end
