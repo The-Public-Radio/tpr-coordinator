afbc15385550a5748dfca95b7c7a03505cc1bae6
Gabe Ochoa <gabeochoa@gmail.com>
2017-07-20 09:31:43 -0400

    More shipment validation, trying to force date object

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,8 +1,14 @@
 require 'tracking_number'
 
 class Shipment < ApplicationRecord
+  def date_object_check
+    errors.add(:ship_date, "Must be a Date object!") unless ship_date.valid_date?
+  end
+
   has_many :radio
   # use tracking_number gem for validation
   validates_presence_of :tracking_number
-  validates :tracking_number, :tracking_number => true
+  validates :tracking_number, tracking_number: true
+  validates_inclusion_of :status, in: %w(created fulfillment shipped)
+  validates_presence_of :ship_date, allow_nil: true
 end
--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Shipment, type: :model do
-  let(:model) { described_class.new(tracking_number: '1Z879E930346834440') }
+  let(:model) { described_class.new(
+    tracking_number: '1Z879E930346834440',
+    status: 'shipped')
+  }
 
   it 'valid with valid attributes' do
     expect(model).to be_valid
@@ -15,5 +18,37 @@ RSpec.describe Shipment, type: :model do
   it 'is not valid with an not valid tracking number' do
     model.tracking_number = 'asdf3t45TRACKINGFAKE'
     expect(model).to_not be_valid
+  end
+
+  it 'is valid when date is nil' do
+    model.ship_date = nil
+    expect(model).to be_valid
+  end
+
+  it 'is valid only when date is a date object or nil' do
+    model.ship_date = Date.new
+    expect(model).to be_valid
+
+    model.ship_date = nil
+    expect(model).to be_valid
+
+    model.ship_date = 'this is not a date'
+    expect(model).to_not be_valid
+  end
+
+  context 'a shipment has a status that' do
+    it 'is valid when shipped, fulfillment, or created' do
+      model.status = 'created'
+      expect(model).to be_valid
+
+      model.status = 'fulfillment'
+      expect(model).to be_valid
+
+      model.status = 'shipped'
+      expect(model).to be_valid
+
+      model.status = 'done'
+      expect(model).to_not be_valid
+    end
   end
 end
