47bb50bd802a1139212e79e01341f7f129fc3976
Gabe Ochoa <gabeochoa@gmail.com>
2017-07-23 17:42:06 -0400

    Adding numerical validation for frequency to radio model

--- app/models/radio.rb
+++ app/models/radio.rb
@@ -1,3 +1,7 @@
 class Radio < ApplicationRecord
   validates_presence_of :frequency
+  validates_length_of :frequency, maximum: 5, message: 'Frequency is not valid: too long!'
+  validates_numericality_of :frequency, on: :create, message: 'Frequency is not valid: only numbers!'
+  validates_numericality_of :frequency, greater_than: 87.5, message: 'Frequency is not valid: the station is less than 87.5!'
+  validates_numericality_of :frequency, less_than: 108, message: 'Frequency is not valid: the station is greater than 108!'
 end
