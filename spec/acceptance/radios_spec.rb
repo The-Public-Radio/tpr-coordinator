a148fd344ea5f1b39e7896464912a6802199e5d6
Gabe Ochoa <gabeochoa@gmail.com>
2017-08-03 15:18:01 -0400

    Moving radio acceptance tests to radio spec

--- spec/acceptance/radios_spec.rb
+++ spec/acceptance/radios_spec.rb
@@ -17,4 +17,36 @@ resource "Radios" do
       expect(data['frequency']).to eq('90.5')
     end
   end
+
+  get "/shipment/:id/radios" do
+    header('Content-Type', 'application/json')
+
+    let(:tracking_number) { '9374889691090496006138' }
+    let(:page) { 2 }
+    parameter :tracking_number, 'String, shipment tracking number', required: true
+
+    example "Look up radios by a shipment's tracking number" do
+      do_request
+      expect(status).to eq 200
+      data = JSON.parse(response_body)['data']
+      expect(data['radios'].length).to eq('2')
+    end
+  end
+
+  get "/shipment/:id/radios" do
+    let(:id) { created_shipment.id }
+    let(:page) { '1' }
+
+    parameter :tracking_number, 'String, shipment tracking number', required: true
+    parameter :page, 'String, page number reqested', requied: true
+
+    example "Paginated radios by a shipment's tracking number" do
+      do_request
+      expect(status).to eq 200
+      data = JSON.parse(response_body)['data']
+      expect(headers['X-Page']).to eq('2')
+      expect(data['radio']['frequency']).to eq('90.5')
+    end
+  end
+
 end
--- spec/acceptance/shipments_spec.rb
+++ spec/acceptance/shipments_spec.rb
@@ -66,32 +66,4 @@ resource "Shipments" do
       expect(data['shipment_status']).to eq(shipment_status)
     end
   end
-
-  get "/shipment/:id/radios" do
-    header('Content-Type', 'application/json')
-
-    let(:tracking_number) { '9374889691090496006138' }
-    let(:page) { 2 }
-    parameter :tracking_number, 'String, shipment tracking number', required: true
-
-    example "Look up radios by a shipment's tracking number" do
-      do_request
-      expect(status).to eq 200
-      data = JSON.parse(response_body)['data']
-      expect(data['radios'].length).to eq('2')
-    end
-  end
-
-  get "/shipment/:id/radios" do
-    parameter :tracking_number, 'String, shipment tracking number', required: true
-    parameter :page, 'String, page number reqested', requied: true
-
-    example "Paginated radios by a shipment's tracking number" do
-      do_request
-      expect(status).to eq 200
-      data = JSON.parse(response_body)['data']
-      expect(headers['X-Page']).to eq('2')
-      expect(data['radio']['frequency']).to eq('90.5')
-    end
-  end
 end
