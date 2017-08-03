4bc0460ddeabcb8a920447b0c67ba09566bbe156
Gabe Ochoa <gabeochoa@gmail.com>
2017-08-03 16:23:00 -0400

    Updating radio routing specs for nested resource

--- spec/routing/radios_routing_spec.rb
+++ spec/routing/radios_routing_spec.rb
@@ -4,29 +4,28 @@ RSpec.describe RadiosController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/radios").to route_to("radios#index")
+      expect(:get => "/shipments/1/radios").to route_to(:controller => "radios", :action => "index", shipment_id: "1" )
     end
 
-
     it "routes to #show" do
-      expect(:get => "/radios/1").to route_to("radios#show", :id => "1")
+      expect(:get => "/shipments/1/radios/1").to route_to("radios#show", :id => "1", shipment_id: "1")
     end
 
 
     it "routes to #create" do
-      expect(:post => "/radios").to route_to("radios#create")
+      expect(:post => "/shipments/1/radios").to route_to("radios#create", shipment_id: "1")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/radios/1").to route_to("radios#update", :id => "1")
+      expect(:put => "/shipments/1/radios/1").to route_to("radios#update", :id => "1", shipment_id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/radios/1").to route_to("radios#update", :id => "1")
+      expect(:patch => "/shipments/1/radios/1").to route_to("radios#update", :id => "1", shipment_id: "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/radios/1").to route_to("radios#destroy", :id => "1")
+      expect(:delete => "/shipments/1/radios/1").to route_to("radios#destroy", :id => "1", shipment_id: "1")
     end
 
   end
