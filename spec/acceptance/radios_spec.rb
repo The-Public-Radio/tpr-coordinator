8d9b608b65a44b71d80764e733cbb101ba01db84
Gabe Ochoa <gabeochoa@gmail.com>
2017-08-07 23:27:44 -0400

    Acceptance test for creating a radio

--- spec/acceptance/radios_spec.rb
+++ spec/acceptance/radios_spec.rb
@@ -10,7 +10,7 @@ resource "Radios" do
   let(:fulfillment_shipment) { create :fulfillment }
   let(:shipment_id) { fulfillment_shipment.id }
   let(:radio) { create(:radio) }
-  let(:radio_frequency) { radio.frequency }
+  let(:frequency) { radio.frequency }
   let(:radio_id) { radio.id }
 
   get "/shipments/:shipment_id/radios/:id" do
@@ -28,7 +28,7 @@ resource "Radios" do
       do_request
       expect(status).to eq 200
       data = JSON.parse(response_body)['data']
-      expect(data[1]['frequency']).to eq(radio_frequency)
+      expect(data[1]['frequency']).to eq(frequency)
       expect(data.length).to be 3
     end
   end 
@@ -39,6 +39,27 @@ resource "Radios" do
 
     example "Look up a shipment's radios one (page) at a time " do
       explanation 'Each page only returns 1 record. The header `X-Total` will give the total number of radios (pages)'
+      do_request
+      expect(status).to eq 200
+      data = JSON.parse(response_body)
+
+      expect(data.count).to be 1
+      expect(data[0]['frequency']).to eq(frequency)
+      expect(response_headers['X-Total']).to eq('3')
+    end
+  end
+
+  post "/radios" do
+    parameter :frequency, 'String, frequency for the radio', required: false
+    parameter :pcb_version, 'String, PCB revision', required: false
+    parameter :serial_number, 'String, radio (speaker) serial number', required: false
+    parameter :operator, 'String, paganation page number', required: false
+
+    let(:pcb_version) { '1' }
+    let(:serial_number) { 'TPRv2.0_1_12345' }
+    let(:operator) { 'Operator McOperatorson' }
+
+    example "Create a radio" do
       do_request
       expect(status).to eq 200
       data = JSON.parse(response_body)
