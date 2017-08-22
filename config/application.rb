1f8a5c963f05d530f73825f73e61e6d0ec96b536
Gabe Ochoa <gabeochoa@gmail.com>
2017-08-21 20:39:47 -0400

    Adding logger

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module TprCoordinator
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.logger = Logger.new(STDOUT)
+    config.log_level = ENV['LOG_LEVEL'] || 'info'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
