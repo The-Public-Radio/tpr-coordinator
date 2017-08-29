42998b5d7538ba3aff8f9c39ebd5e74a3ffa2e10
Gabe Ochoa <gabeochoa@gmail.com>
2017-08-29 11:33:46 -0400

    Reworking Seeds file for good data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,118 +9,33 @@ if Rails.env.production?
   raise 'DO NOT DO THIS IN PRODUCTION'
 end
 
-radios = [
-	{
-		frequency: '98.1',
-		pcb_version: '1',
-		serial_number: 'TPRv2.0_1_12345',
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-		shipment_id: 22
-	},
-	{
-		frequency: '79.3',
-		pcb_version: '1',
-		serial_number: 'TPRv2.0_1_12345',
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-		shipment_id: 22
-	},	
-	{
-		frequency: '103.1',
-		pcb_version: '1',
-		serial_number: 'TPRv2.0_1_12345',
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-		shipment_id: 22
-	},
-	{
-		frequency: '103.1',
-		pcb_version: '1',
-		serial_number: 'TPRv2.0_1_12345',
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-		shipment_id: 22
-	},
-	{
-		frequency: '103.1',
-		pcb_version: '1',
-		serial_number: 'TPRv2.0_1_12345',
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-		shipment_id: 23
-	},
-	{
-		frequency: nil,
-		pcb_version: '1',
-		serial_number: 'TPRv2.0_1_12345',
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-		shipment_id: nil
-	},
-	{
-		frequency: nil,
-		pcb_version: '1',
-		serial_number: 'TPRv2.0_1_12345',
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-		shipment_id: nil
-	},
-	{
-		frequency: '101.1',
-		pcb_version: '1',
-		serial_number: 'TPRv2.0_1_92007',
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-		shipment_id: 25,
-		boxed: false
-	},
-	{
-		frequency: '97.1',
-		pcb_version: '1',
-		serial_number: 'TPRv2.0_1_49079',
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-		shipment_id: 25,
-		boxed: false
-	},
-	{
-		frequency: '93.9',
-		pcb_version: '1',
-		serial_number: 'TPRv2.0_1_53012',
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-		shipment_id: 25,
-		boxed: false
-	}]
+def random_tpr_serial_number
+  "TPRv2.0_1_#{Random.new.rand(99999)}"
+end
 
-shipments = [
-	{
-		tracking_number: '9374889691090496850816',
-		shipment_status: 'created',
-		ship_date: Time.now,
-		order_id: 24
-	},
-	{
-		tracking_number: '9937488969109049676230',
-		shipment_status: 'fulfillment',
-		ship_date: Time.now,
-		order_id: 24
-	},
-	{
-		tracking_number: '9374889691090496296829',
-		shipment_status: 'shipped',
-		ship_date: Time.now,
-		order_id: 23
-	},
-	{
-		tracking_number: '9400111298370828166024',
-		shipment_status: 'shipped',
-		ship_date: Time.now,
-		order_id: 23
-	}]
+def random_frequency
+  Random.new.rand(78.0..108.0).round(1)
+end
 
-orders = [
+def random_tracking_number
+  number = "9374889691090496#{Random.new.rand(99999)}"
+  chars = number.gsub(/^420\d{5}/, '').chars.to_a
+
+  total = 0
+  chars.reverse.each_with_index do |c, i|
+    x = c.to_i
+    x *= 3 if i.even?
+
+    total += x
+  end
+
+  check_digit = total % 10
+  check_digit = 10 - check_digit unless (check_digit.zero?)
+  # TrackingNumber.new("#{number}#{check_digit}")
+  "#{number}#{check_digit}"
+end
+
+orders_params = [
 	{
 		first_name: 'Person',
 		last_name: 'McPersonson',
@@ -143,6 +58,167 @@ orders = [
 		email: 'Person.McPersonson@gmail.com'
 	}]
 
-orders.each{ |order| Order.create(order) }
-shipments.each{ |order| Shipment.create(order) }
-radios.each{ |order| Radio.create(order) }
+created_orders = []
+orders_params.each{ |params| created_orders << Order.create(params) }
+
+shipments_params = [
+	{
+		tracking_number: random_tracking_number,
+		shipment_status: 'created',
+		ship_date: Time.now,
+		order_id: created_orders[0].id
+	},
+  {
+		tracking_number: random_tracking_number,
+		shipment_status: 'label_created',
+		ship_date: Time.now,
+		order_id: created_orders[0].id
+	},
+	# This shipment is an actual tracking number and not a randomly generated one
+	{
+		tracking_number: '9400111298370828166024',
+		shipment_status: 'label_created',
+		ship_date: Time.now,
+		order_id: created_orders[1].id
+	},
+	{
+		tracking_number: random_tracking_number,
+		shipment_status: 'boxed',
+		ship_date: Time.now,
+		order_id: created_orders[2].id
+	},
+	{
+		tracking_number: random_tracking_number,
+		shipment_status: 'shipped',
+		ship_date: Time.now,
+		order_id: created_orders[2].id
+	}]
+
+created_shipments = []
+shipments_params.each{ |params| created_shipments << Shipment.create(params) }
+
+radios_params = [
+	# Radios created from orders params / customer orders
+	# 
+	# The first 3 here have known NYC frequencies for testing, the rest are random
+  {
+		frequency: '93.9',
+		shipment_id: created_shipments[2].id,
+		country_code: 'us'
+	},
+	{
+		frequency: '97.1',
+		shipment_id: created_shipments[2].id,
+		country_code: 'us'
+	},	
+	{
+		frequency: '101.1',
+		shipment_id: created_shipments[2].id,
+		country_code: 'us'
+	},
+	{
+		frequency: random_frequency,
+		shipment_id: created_shipments[3].id,
+		country_code: 'us'
+	},
+	{
+		frequency: random_frequency,
+		shipment_id: created_shipments[3].id,
+		country_code: 'us'
+	},
+	{
+		frequency: random_frequency,
+		shipment_id: created_shipments[1].id,
+		country_code: 'us'
+	},
+	{
+		frequency: random_frequency,
+		shipment_id: created_shipments[0].id,
+		country_code: 'us'
+	},
+	{
+		frequency: random_frequency,
+		shipment_id: created_shipments[0].id,
+		country_code: 'us'
+	},
+
+
+	# Assembled Radios
+	{
+		pcb_version: '1',
+		serial_number: random_tpr_serial_number,
+		assembly_date: Time.now,
+		operator: 'Person McPersonson',
+	},
+	{
+		pcb_version: '1',
+		serial_number: random_tpr_serial_number,
+		assembly_date: Time.now,
+		operator: 'Person McPersonson',
+	},	
+	{
+		pcb_version: '1',
+		serial_number: random_tpr_serial_number,
+		assembly_date: Time.now,
+		operator: 'Person McPersonson',
+	},
+	{
+		pcb_version: '1',
+		serial_number: random_tpr_serial_number,
+		assembly_date: Time.now,
+		operator: 'Person McPersonson',
+	},
+	{
+		pcb_version: '1',
+		serial_number: random_tpr_serial_number,
+		assembly_date: Time.now,
+		operator: 'Person McPersonson',
+	},
+	{
+		pcb_version: '1',
+		serial_number: random_tpr_serial_number,
+		assembly_date: Time.now,
+		operator: 'Person McPersonson',
+	},
+	{
+		pcb_version: '1',
+		serial_number: random_tpr_serial_number,
+		assembly_date: Time.now,
+		operator: 'Person McPersonson',
+	},
+
+
+	# Boxed Radios, assigned to a shipment and frequency
+	{
+		frequency: random_frequency,
+		pcb_version: '1',
+		serial_number: random_tpr_serial_number,
+		assembly_date: Time.now,
+		operator: 'Person McPersonson',
+		shipment_id: shipments[4].id,
+		country_code: 'us',
+		boxed: true
+	},
+	{
+		frequency: random_frequency,
+		pcb_version: '1',
+		serial_number: random_tpr_serial_number,
+		assembly_date: Time.now,
+		operator: 'Person McPersonson',
+		shipment_id: shipments[4].id,
+		country_code: 'us',
+		boxed: true
+	},
+	{
+		frequency: random_frequency,
+		pcb_version: '1',
+		serial_number: random_tpr_serial_number,
+		assembly_date: Time.now,
+		operator: 'Person McPersonson',
+		shipment_id: shipments[4].id,
+		country_code: 'us',
+		boxed: true
+	}
+]
+
+radios_params.each{ |params| Radio.create(params) }
