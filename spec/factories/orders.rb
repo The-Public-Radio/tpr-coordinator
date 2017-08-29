d693df70514ec761ed193260fe3946b4160abf78
Gabe Ochoa <gabeochoa@gmail.com>
2017-08-29 16:37:24 -0400

    Updating order factory with new address fields

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,7 +2,13 @@ FactoryGirl.define do
   factory :order do
     first_name "Spencer"
     last_name "Right"
-    address "123 West 9th St., City, State, USA"
+    street_address_1 "123 West 9th St."
+    street_address_2 'Apt 4'
+    city 'Brooklyn'
+    state 'NY'
+    postal_code '11221'
+    country 'US'
+    phone '123-321-1231'
     order_source 'other'
     email { "#{first_name}.#{last_name}@gmail.com" }
     
