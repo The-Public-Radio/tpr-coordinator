7529b50d8768f7399759e8071024384996af0da4
Gabe Ochoa <gabeochoa@gmail.com>
2017-09-04 20:53:16 -0400

    Revert "Terrible. Make sure to revert this. It's not tested, it's ill-advised, and people should handle their null values"
    This reverts commit 32b76a309e24b3e017b9a6483f4c50ddce4687b6.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,10 @@ class ApplicationController < ActionController::API
 
   def api_response(data, status = :ok, errors = [])
     serializer = ActiveModel::Serializer.serializer_for(data)
-    if serializer.nil?
-      ret = {
-        'data' => data,
-        'errors' => Array.wrap(errors)
-      }
-    else
-      ret = {
-        'data' => serializer.try(:new, data),
-        'errors' => Array.wrap(errors)
-      }
-    end
+    ret = {
+      'data' => serializer.try(:new, data),
+      'errors' => Array.wrap(errors)
+    }
     Rails.logger.debug("Status: #{status}")
     Rails.logger.debug("Body: #{ret}")
     render json: ret, status: status
