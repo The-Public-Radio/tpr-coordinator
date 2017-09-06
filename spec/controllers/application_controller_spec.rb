e11e138fc510f0166bbd5726ac9d2279b5674031
Gabe Ochoa <gabeochoa@gmail.com>
2017-09-05 21:44:11 -0400

    Test reminder for refusing http com

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
-
+ it 'Reject all traffic over http' do
+ 	skip('Write this!')
+ 	# Heroku header to check for:
+ 	# X-Forwarded-Proto
+ end
 end
