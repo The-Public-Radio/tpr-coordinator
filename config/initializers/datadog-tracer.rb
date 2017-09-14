3aa4d11875ec7682fb9ca9e899c47d235abe5121
Gabe Ochoa <gabeochoa@gmail.com>
2017-09-14 00:16:42 -0400

    Config dd via env

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -1,5 +1,8 @@
 Rails.configuration.datadog_trace = {
   auto_instrument: true,
   auto_instrument_redis: true,
-  default_service: 'tpr-coordinator'
+  default_service: 'tpr-coordinator',
+  trace_agent_hostname: ENV['DD_TRACE_AGENT_HOSTNAME'] || 'localhost',
+  trace_agent_port: ENV['DD_TRACE_AGENT_PORT'] || 8126,
+  env: nil
 }
