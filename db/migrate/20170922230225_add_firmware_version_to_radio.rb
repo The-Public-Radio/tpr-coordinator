588fa5f205851ddc7bf6f2c6558feaa350f16965
Gabe Ochoa <gochoa@squarespace.com>
2017-09-22 19:18:20 -0400

    Adding currenrt sha1 of firmware git repo as initial firmware_version

--- db/migrate/20170922230225_add_firmware_version_to_radio.rb
+++ db/migrate/20170922230225_add_firmware_version_to_radio.rb
@@ -3,7 +3,7 @@ class AddFirmwareVersionToRadio < ActiveRecord::Migration[5.1]
     add_column :radios, :firmware_version, :string
 
     Radio.all.each do |radio|
-      radio.firmware_version = '1'
+      radio.firmware_version = 'a10fde1a52063d7022efb00924f25e9d915fc66c'
       radio.save!
     end
   end
