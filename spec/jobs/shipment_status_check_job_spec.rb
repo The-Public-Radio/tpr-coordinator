1caf5d8a16bd1c021b68279f5021027981e9d829
Gabe Ochoa <gabeochoa@gmail.com>
2017-10-12 09:32:19 -0400

    Initial stubbing of shipmnent_status job

--- spec/jobs/shipment_status_check_job_spec.rb
+++ spec/jobs/shipment_status_check_job_spec.rb
@@ -10,10 +10,91 @@ RSpec.describe ShipmentStatusCheckJob, type: :job do
   end
 
   describe "#check_status" do
+  	# 'UNKNOWN', 'DELIVERED', 'TRANSIT', 'FAILURE', 'RETURNED'.
+  	let(:shippo_not_shipped_response) { 
+  		{
+			    "object_state":"VALID",
+			    "status":"SUCCESS",
+			    "object_created":"2014-11-29T16:31:19.512Z",
+			    "object_updated":"2014-11-29T16:31:19.512Z",
+			    "object_id":"5695ae3a5eda41ba9abdbf347fd545f3",
+			    "tracking_number":"9102969010383081813033",
+			    "tracking_status":{  
+			        "object_created":"2014-11-29T16:31:19.511Z",
+			        "status":"UNKNOWN",
+			        "status_details":"Your shipment has been delivered.",
+			        "status_date":"2012-03-08T09:58:00Z",
+			        "location":{  
+			            "city":"Beverly Hills",
+			            "state":"CA",
+			            "zip":"90210",
+			            "country":"US"
+			        }
+			    },
+			    "eta":"2014-07-21T12:00:00.000Z"
+			}
+  	 }
+
+  	let(:shippo_delivered_response) {
+  		{
+			    "object_state":"VALID",
+			    "status":"SUCCESS",
+			    "object_created":"2014-11-29T16:31:19.512Z",
+			    "object_updated":"2014-11-29T16:31:19.512Z",
+			    "object_id":"5695ae3a5eda41ba9abdbf347fd545f3",
+			    "tracking_number":"9102969010383081813033",
+			    "tracking_status":{  
+			        "object_created":"2014-11-29T16:31:19.511Z",
+			        "status":"DELIVERED",
+			        "status_details":"Your shipment has been delivered.",
+			        "status_date":"2012-03-08T09:58:00Z",
+			        "location":{  
+			            "city":"Beverly Hills",
+			            "state":"CA",
+			            "zip":"90210",
+			            "country":"US"
+			        }
+			    },
+			    "eta":"2014-07-21T12:00:00.000Z"
+			}
+  	}
+
+  	let(:shippo_picked_up_response) {
+  		{
+			    "object_state":"VALID",
+			    "status":"SUCCESS",
+			    "object_created":"2014-11-29T16:31:19.512Z",
+			    "object_updated":"2014-11-29T16:31:19.512Z",
+			    "object_id":"5695ae3a5eda41ba9abdbf347fd545f3",
+			    "tracking_number":"9102969010383081813033",
+			    "tracking_status":{  
+			        "object_created":"2014-11-29T16:31:19.511Z",
+			        "status":"TRANSIT",
+			        "status_details":"Your shipment has been delivered.",
+			        "status_date":"2012-03-08T09:58:00Z",
+			        "location":{  
+			            "city":"Beverly Hills",
+			            "state":"CA",
+			            "zip":"90210",
+			            "country":"US"
+			        }
+			    },
+			    "eta":"2014-07-21T12:00:00.000Z"
+			}
+  	}
+
+  	let(:shippo_transactions_url) { 'https://api.goshippo.com/transactions/' }
+  	let(:shippo_headers) { 'HEADERS' }
   	it "calls the shippo api to check the status of each shipment" do
-	  	skip('write this')
+	  	
+	  	shippo_params = {
+
+	  	}
+	  	
+	  	expect(HTTParty).to recieve(:get).with(shippo_transactions_url, shippo params).and_return
+	  	ShipmentStatusCheckJob.check_status
 	  end
-	  
+
 	  it "updates the shipment_status to delivered if the package has been delivered" do
 	  	skip('write this')
 	  end
