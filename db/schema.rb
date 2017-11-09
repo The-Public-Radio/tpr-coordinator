41b072577a01e71e2b25da2b71afacc19f2f9438
Gabe Ochoa <gabeochoa@gmail.com>
2017-11-08 22:10:28 -0500

    Adding fields to order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030122137) do
+ActiveRecord::Schema.define(version: 20171109030850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20171030122137) do
     t.string "country"
     t.string "phone"
     t.string "name"
+    t.boolean "invoiced"
+    t.string "reference_number"
   end
 
   create_table "radios", force: :cascade do |t|
