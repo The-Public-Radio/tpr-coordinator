0ed324fbc02d5c26839624d185cd446ad19cc5cd
Gabe Ochoa <gabeochoa@gmail.com>
2018-05-19 20:38:06 -0400

    Add shippo_reference_id to shipment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317162030) do
+ActiveRecord::Schema.define(version: 20180520003611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20180317162030) do
     t.string "label_url"
     t.string "shipment_priority"
     t.string "return_label_url"
+    t.string "shippo_reference_id"
   end
 
   create_table "users", force: :cascade do |t|
