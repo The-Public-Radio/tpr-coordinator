97f0b85ab9cf3cea9caee077d13e980d7ba3149e
Gabe Ochoa <gabeochoa@gmail.com>
2018-06-02 21:19:40 -0400

    Remove large order acceptance test in favor of controller tests

--- spec/acceptance/orders_spec.rb
+++ spec/acceptance/orders_spec.rb
@@ -51,41 +51,4 @@ resource "Orders" do
       end
     end
   end
-
-  post '/orders' do
-    parameter :frequencies, 'String array, frequencies requested. Each entity in the array corresponds to a single radio', required: true
-    parameter :name, 'String, first name for the order', required: true
-    parameter :address, 'String, address where the order should be shipped', required: true
-    parameter :order_source, 'String, where the order came from. Options: kickstarter, squarespace, other', required: false
-
-    let(:tracking_number) { random_tracking_number }
-
-    example 'Create a new order' do
-      order_params = {
-        order_source: 'other',
-        name: random_name,
-        address: '345 West Way, Brooklyn, NY, 11221',
-        frequencies: {
-          'us': ['98.3', '79.5', '79.5', '98.3', '79.5', '79.5', '98.3', '79.5', '79.5', '105.6']
-          },
-        email: 'person.mcpersonson@gmail.com'
-      }
-
-      shippo_response_object = object_double('shippo response', code: 200, 
-        status: 'SUCCESS', success?: true, tracking_number: '9400111298370829688891', 
-        label_url: 'https://shippo-delivery-east.s3.amazonaws.com/some_label.pdf')
-
-      expect(Shippo::Transaction).to receive(:create).and_return(shippo_response_object).exactly(4)
-      expect(shippo_response_object).to receive(:[]).with('status').and_return('SUCCESS').exactly(4)
-      expect(shippo_response_object).to receive(:tracking_number).and_return('9400111298370829688891').exactly(4)
-      expect(shippo_response_object).to receive(:label_url).and_return('https://shippo-delivery-east.s3.amazonaws.com/some_label.pdf').exactly(4)
-      expect{ do_request(order_params) }.to change(Order, :count).by(1)
-      expect(status).to be 201
-
-      data = JSON.parse(response_body)
-      expect(data['order_source']).to eq(order_params[:order_source])
-      expect(data['name']).to eq(order_params[:name])
-      expect(data['email']).to eq(order_params[:email])
-    end
-  end
 end
