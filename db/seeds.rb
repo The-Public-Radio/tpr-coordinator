447222490c29bb3816479117523c022fdecc50a4
Spencer Wright <snwright@gmail.com>
2019-01-09 00:54:30 -0500

    updating seeds for QBO testing
    these seeds will be used to test: "orders that have shipments that are 'label_created' do NOT get invoices"

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,38 +39,67 @@ def random_name
 end
 
 orders_params = [
+	# 1-line economy order
 	{
-	    name: random_name,
-		order_source: 'kickstarter',
-		street_address_1: "123 West 9th St.",
-	    street_address_2: 'Apt 4',
+	    name: 'Sally Person',
+		order_source: 'uncommon_goods',
+		street_address_1: "544 Park Ave",
+	    street_address_2: 'Ste 332',
 	    city: 'Brooklyn',
 	    state: 'NY',
-	    postal_code: '11221',
+	    postal_code: '11205',
 	    country: 'US',
 	    phone: '123-321-1231',
 		email: 'Person.McPersonson@gmail.com'
 	},
+	# 2-line economy order
 	{
 	    name: 'John Person',
-	    order_source: 'other',
-	    street_address_1: "100 main st",
-	    street_address_2: '',
-	    city: 'arlington',
-	    state: 'MA',
-	    postal_code: '02476',
+	    order_source: 'uncommon_goods',
+	    street_address_1: "544 Park Ave",
+	    street_address_2: 'Ste 332',
+	    city: 'Brooklyn',
+	    state: 'NY',
+	    postal_code: '11205',
 	    country: 'US',
 	    phone: '123-321-1231',
 	    email: 'Person.McPersonson@gmail.com'
 	},
+	# 3-line economy order
 	{
-	    name: random_name,
-		order_source: 'other',
-		street_address_1: "123 West 9th St.",
-	    street_address_2: 'Apt 2',
+	    name: 'Jamie Person',
+	    order_source: 'uncommon_goods',
+	    street_address_1: "544 Park Ave",
+	    street_address_2: 'Ste 332',
 	    city: 'Brooklyn',
 	    state: 'NY',
-	    postal_code: '11221',
+	    postal_code: '11205',
+	    country: 'US',
+	    phone: '123-321-1231',
+	    email: 'Person.McPersonson@gmail.com'
+	},
+	# 7-line economy order
+	{
+	    name: 'Peter Person',
+	    order_source: 'uncommon_goods',
+	    street_address_1: "544 Park Ave",
+	    street_address_2: 'Ste 332',
+	    city: 'Brooklyn',
+	    state: 'NY',
+	    postal_code: '11205',
+	    country: 'US',
+	    phone: '123-321-1231',
+	    email: 'Person.McPersonson@gmail.com'
+	},
+	# "other" order
+	{
+	    name: 'Susan Person',
+		order_source: 'other',
+		street_address_1: "544 Park Ave",
+	    street_address_2: 'Ste 332',
+	    city: 'Brooklyn',
+	    state: 'NY',
+	    postal_code: '11205',
 	    country: 'US',
 	    phone: '123-321-1231',
 		email: 'Person.McPersonson@gmail.com'
@@ -81,50 +110,49 @@ created_orders = []
 orders_params.each{ |params| created_orders << Order.create(params) }
 
 shipments_params = [
+	# Order 0 gets 1 shipment
 	{
 		tracking_number: random_tracking_number,
-		shipment_status: 'created',
+		shipment_status: 'label_created',
 		ship_date: Time.now,
 		order_id: created_orders[0].id
 	},
+	# Order 1 gets 1 shipment
 	{
 		tracking_number: random_tracking_number,
-		shipment_status: 'label_printed',
-		ship_date: Time.now,
-		order_id: created_orders[0].id
-	},
-	{
-		tracking_number: 'LZ780632087US',
 		shipment_status: 'label_created',
 		ship_date: Time.now,
 		order_id: created_orders[1].id
 	},
+	# Order 2 gets 1 shipment
 	{
 		tracking_number: random_tracking_number,
-		shipment_status: 'boxed',
-		ship_date: Time.now,
-		order_id: created_orders[2].id
-	},
-	{
-		tracking_number: random_tracking_number,
-		shipment_status: 'boxed',
-		ship_date: Time.now,
-		order_id: created_orders[2].id
-	},
-	{
-		tracking_number: '92055901755477000027167920',
 		shipment_status: 'label_created',
 		ship_date: Time.now,
 		order_id: created_orders[2].id
 	},
+	# Order 3 gets 3 shipments (two 3-packs, one single)
 	{
-	    tracking_number: '92001901755477000008242445',
-	    shipment_status: 'label_created',
-	    ship_date: Time.now,
-	    order_id: created_orders[3].id
+		tracking_number: random_tracking_number,
+		shipment_status: 'label_created',
+		ship_date: Time.now,
+		order_id: created_orders[3].id
 	},
 	{
-		tracking_number: '92055901755477000027167937',
+		tracking_number: random_tracking_number,
+		shipment_status: 'label_created',
+		ship_date: Time.now,
+		order_id: created_orders[3].id
+	},
+	{
+		tracking_number: random_tracking_number,
+		shipment_status: 'label_created',
+		ship_date: Time.now,
+		order_id: created_orders[3].id
+	},
+	# Order 4 gets 1 shipment
+	{
+		tracking_number: random_tracking_number,
 		shipment_status: 'label_created',
 		ship_date: Time.now,
 		order_id: created_orders[4].id
@@ -137,50 +165,45 @@ shipments_params.each{ |params| created_shipments << Shipment.create(params) }
 radios_params = [
 	# Radios created from orders params / customer orders
 	#
-	# shipment 2 should have one radio
-  	# shipment 5 should have two radios
- 	# shipment 6 should have one radio
-  	# shipment 7 should have three radios
+	# Shipment 0 gets 1 radio
+	# Shipment 1 gets 2 radios
+	# Shipment 2 gets 3 radios
+	# Shipment 3 gets 7 radios
+	# Shipment 4 gets 1 radio
   	#
 	{
 		frequency: '91.5',
+		shipment_id: created_shipments[0].id,
+		country_code: 'US',
+		boxed: false
+	},
+	{
+		frequency: '107.5',
+		shipment_id: created_shipments[1].id,
+		country_code: 'US',
+		boxed: false
+	},
+	{
+		frequency: '97.1',
+		shipment_id: created_shipments[1].id,
+		country_code: 'US',
+		boxed: false
+	},
+	{
+		frequency: '107.5',
 		shipment_id: created_shipments[2].id,
 		country_code: 'US',
 		boxed: false
 	},
 	{
-		frequency: '107.5',
-		shipment_id: created_shipments[5].id,
-		country_code: 'US',
-		boxed: false
-	},
-	{
-		frequency: '97.1',
-		shipment_id: created_shipments[5].id,
-		country_code: 'US',
-		boxed: false
-	},
-	{
-		frequency: '107.5',
-		shipment_id: created_shipments[6].id,
-		country_code: 'US',
-		boxed: false
-	},
-	{
     	frequency: '97.1',
-		shipment_id: created_shipments[7].id,
+		shipment_id: created_shipments[2].id,
 		country_code: 'US',
 		boxed: false
 	},
 	{
 		frequency: '97.1',
-		shipment_id: created_shipments[7].id,
-		country_code: 'US',
-		boxed: false
-	},
-	{
-		frequency: random_frequency,
-		shipment_id: created_shipments[7].id,
+		shipment_id: created_shipments[2].id,
 		country_code: 'US',
 		boxed: false
 	},
@@ -192,120 +215,81 @@ radios_params = [
 	},
 	{
 		frequency: random_frequency,
-		shipment_id: created_shipments[1].id,
+		shipment_id: created_shipments[3].id,
 		country_code: 'US',
 		boxed: false
 	},
 	{
 		frequency: random_frequency,
-		shipment_id: created_shipments[0].id,
+		shipment_id: created_shipments[3].id,
 		country_code: 'US',
 		boxed: false
 	},
 	{
 		frequency: random_frequency,
-		shipment_id: created_shipments[0].id,
+		shipment_id: created_shipments[3].id,
+		country_code: 'US',
+		boxed: false
+	},
+	{
+		frequency: random_frequency,
+		shipment_id: created_shipments[3].id,
+		country_code: 'US',
+		boxed: false
+	},
+	{
+		frequency: random_frequency,
+		shipment_id: created_shipments[3].id,
+		country_code: 'US',
+		boxed: false
+	},
+	{
+		frequency: random_frequency,
+		shipment_id: created_shipments[3].id,
+		country_code: 'US',
+		boxed: false
+	},
+	{
+		frequency: random_frequency,
+		shipment_id: created_shipments[4].id,
 		country_code: 'US',
 		boxed: false
 	},
 
-
-	# Assembled Radios
-	#
-	# First assembled radio has a known serial nubmer for testing
-	{
-		pcb_version: 'PR9026',
-		serial_number: 'TPRv2.0_1_63297',
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-	    quality_control_status: 'passed',
-	    firmware_version: 'a10fde1a52063d7022efb00924f25e9d915fc66c'
-	},
-	{
-		pcb_version: 'PR9026',
-		serial_number: random_tpr_serial_number,
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-	    quality_control_status: 'passed',
-	    firmware_version: 'a10fde1a52063d7022efb00924f25e9d915fc66c'
-	},
-	{
-		pcb_version: 'PR9026',
-		serial_number: random_tpr_serial_number,
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-	    quality_control_status: 'passed',
-	    firmware_version: 'a10fde1a52063d7022efb00924f25e9d915fc66c'
-	},
-	{
-		pcb_version: 'PR9026',
-		serial_number: random_tpr_serial_number,
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-	    quality_control_status: 'passed',
-	    firmware_version: 'a10fde1a52063d7022efb00924f25e9d915fc66c'
-	},
-	{
-		pcb_version: 'PR9026',
-		serial_number: random_tpr_serial_number,
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-	    quality_control_status: 'failed_mech',
-	},
-	{
-		pcb_version: 'PR9026',
-		serial_number: random_tpr_serial_number,
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-	    quality_control_status: 'failed_software',
-	    firmware_version: 'a10fde1a52063d7022efb00924f25e9d915fc66c'
-	},
-	{
-		pcb_version: 'PR9026',
-		serial_number: random_tpr_serial_number,
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-	    quality_control_status: 'failed_software',
-	    firmware_version: 'a10fde1a52063d7022efb00924f25e9d915fc66c'
-	},
 
 	# Boxed Radios, assigned to a shipment and frequency
+#	{
+#		frequency: random_frequency,
+#		pcb_version: 'PR9026',
+#		serial_number: random_tpr_serial_number,
+#		assembly_date: Time.now,
+#		operator: 'Person McPersonson',
+#		shipment_id: created_shipments[4].id,
+#		country_code: 'US',
+#		boxed: true,
+#	    quality_control_status: 'passed',
+#	    firmware_version: 'a10fde1a52063d7022efb00924f25e9d915fc66c'
+#	},
+
+
+	# Assembled Radios in inventory
 	{
-		frequency: random_frequency,
 		pcb_version: 'PR9026',
 		serial_number: random_tpr_serial_number,
 		assembly_date: Time.now,
 		operator: 'Person McPersonson',
-		shipment_id: created_shipments[4].id,
-		country_code: 'US',
-		boxed: true,
 	    quality_control_status: 'passed',
 	    firmware_version: 'a10fde1a52063d7022efb00924f25e9d915fc66c'
 	},
 	{
-		frequency: random_frequency,
 		pcb_version: 'PR9026',
 		serial_number: random_tpr_serial_number,
 		assembly_date: Time.now,
 		operator: 'Person McPersonson',
-		shipment_id: created_shipments[4].id,
-		country_code: 'US',
-		boxed: true,
-	    quality_control_status: 'passed',
-	    firmware_version: 'a10fde1a52063d7022efb00924f25e9d915fc66c'
-	},
-	{
-		frequency: random_frequency,
-		pcb_version: 'PR9026',
-		serial_number: random_tpr_serial_number,
-		assembly_date: Time.now,
-		operator: 'Person McPersonson',
-		shipment_id: created_shipments[4].id,
-		country_code: 'US',
-		boxed: true,
 	    quality_control_status: 'passed',
 	    firmware_version: 'a10fde1a52063d7022efb00924f25e9d915fc66c'
 	}
+
 ]
 
 radios_params.each{ |params| Radio.create(params) }
